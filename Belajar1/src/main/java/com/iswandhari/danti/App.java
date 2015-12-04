3d005278863869519a6a82129f0fd62f9358b04c
dantiiswand <danti2896@gmail.com>
2015-12-04 14:38:09 +0700

    Test

---
+++ Belajar1/src/main/java/com/iswandhari/danti/App.java
@@ -0,0 +1,13 @@
+package com.iswandhari.danti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
